8f343739c6bdd49d931aa1a73b00465a8fc8f8c9
JamelaP <jamelapeterson@gmail.com>
2022-05-23 20:59:55 -0700

    'Correct variable reference order'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int n;
+	int L;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = (n) % 10;
+	if (L > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, L);
+	}
+	if (L == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, L);
+	}
+	if (L < 6 &&  L != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, L);
+	}
 	return (0);
 }
