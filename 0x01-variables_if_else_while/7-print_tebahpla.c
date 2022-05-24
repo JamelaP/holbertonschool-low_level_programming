e9442f36422e90491e6e3895648cc74d4b8a7f32
JamelaP <jamelapeterson@gmail.com>
2022-05-24 05:10:21 -0700

    'corrected the expression'

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int a;
+
+	for (a = 122; a > 96; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
