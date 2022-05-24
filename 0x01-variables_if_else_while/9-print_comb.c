e1c74d897ae12c26adb4e548938993b9c818cad6
JamelaP <jamelapeterson@gmail.com>
2022-05-24 15:19:06 -0700

    'Corrected output with if statement'

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,15 @@ int main(void)
 {
 	int a;
 
-	for (a = 48; a < 58; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
+
+		if (a < 57)
+		{
 		putchar(44);
 		putchar(32);
+		}
 	}
 	putchar('\n');
 	return (0);
