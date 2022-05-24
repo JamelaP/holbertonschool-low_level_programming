0e33bd1f586091e897f776136dd045a92cd12c25
JamelaP <jamelapeterson@gmail.com>
2022-05-24 05:54:57 -0700

    'for loops for base16'

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int a;
+
+	for (a = 48; a < 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
