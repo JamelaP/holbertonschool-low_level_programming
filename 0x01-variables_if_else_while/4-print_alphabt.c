e07db1dbaa69f7073ad2313af0e6228307e4a3b8
JamelaP <jamelapeterson@gmail.com>
2022-05-24 04:49:41 -0700

    'Corrected betty and nested loop'

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int a;
+
+	a = 65;
+
+	for (a = 97; a < 123; a++)
+	{
+		if (a != 101 && a != 113)
+		{
+		putchar(a);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
