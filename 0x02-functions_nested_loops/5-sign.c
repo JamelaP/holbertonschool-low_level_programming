49d7343767c45441c13aa26cbe87308f565f6259
JamelaP <jamelapeterson@gmail.com>
2022-05-26 15:05:16 -0700

    'Multiple if statements'

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number.
+ *
+ * @n: = input variable
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar(48);
+	}
+	return (0);
+}
+
