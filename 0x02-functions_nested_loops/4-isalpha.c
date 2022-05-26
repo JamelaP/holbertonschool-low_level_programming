c3ca2a7deca00a83ef939a84699f9cbc9cbeb7ce
JamelaP <jamelapeterson@gmail.com>
2022-05-26 14:34:47 -0700

    'Add two if statements'

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha  - checks for lowercase character
+*
+* @c: is input
+*
+* Return: Always 0.
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
