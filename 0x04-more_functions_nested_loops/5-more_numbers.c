719df9f4866e6997be3a3efb84c6864589a7b3fe
JamelaP <jamelapeterson@gmail.com>
2022-05-31 21:07:10 -0700

    'nested loops'

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,19 @@
 
 void more_numbers(void)
 {
-	if (c >= 'A' && c <= 'Z')
+	int j;
+	int k;
+
+	for (j = 0; j < 10; j++)
 	{
-		return (1);
+		for (k = 0; k <= 14; k++)
+		{
+			if (k >= 9)
+			{
+			_putchar(k / 10 + '0');
+			}
+			_putchar(k % 10 + '0');
+		}
+		_putchar('\n');
 	}
 }
