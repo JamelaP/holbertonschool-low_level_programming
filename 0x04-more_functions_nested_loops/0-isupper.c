49c656cb66d2039afa5f4825258e00d93d18217b
JamelaP <jamelapeterson@gmail.com>
2022-05-31 17:45:40 -0700

    'if Statement Question 0'

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * _isupper - check the code.
+ *
+ * @c: input
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+{
+    	if ( c >= 'A' && c <= 'Z')
+	{ 
+		return (1);
+	}
+	return (0);
 }
