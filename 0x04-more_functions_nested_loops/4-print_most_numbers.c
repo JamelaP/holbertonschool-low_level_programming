66c39dd07b45a90ef0537c11b703bdb2d6358fec
JamelaP <jamelapeterson@gmail.com>
2022-05-31 20:42:05 -0700

    'Print without 4 & 5'

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,14 @@
 
 void print_most_numbers(void)
 {
-	if (c >= 'A' && c <= 'Z')
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-		return (1);
+		if (j != '2' || j != '4')
+		{
+			_putchar(j + '0');
+		}
 	}
+	_putchar('\n');
 }
