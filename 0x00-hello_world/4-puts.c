0f3ccf374c5c4de10282f9b3de58ef37026df5f9
JamelaP <jamelapeterson@gmail.com>
2022-06-03 20:13:10 -0700

    'Regrade Catch-Up Work'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - use puts for quote
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
+	Puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - printf quote
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
