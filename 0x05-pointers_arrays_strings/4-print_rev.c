0c0a34e5d0ba596ce508191f4b1b67e2ebf740ff
JamelaP <jamelapeterson@gmail.com>
2022-06-03 19:23:56 -0700

    'All Files Created'

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ *
+ *@n: input
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int n = 98;
+	int *p = &n;
+	*p = 402;
+}
