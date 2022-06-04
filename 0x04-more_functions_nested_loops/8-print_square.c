dc49fe906a47593ccb42001e0fd9eb466d7b4a82
JamelaP <jamelapeterson@gmail.com>
2022-06-03 22:00:07 -0700

    'compiles file'

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,23 +11,6 @@
 
 void print_square(int size)
 {
-	int w;
-	int t;
-
-	if (size > 0)
-	{
-		for (t = 0; t < size; t++)
-		{
-			for (w = 0; w < size; w++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar(size);
 
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - check the code
- *
- *@n: input
- *
- * Return: Always 0.
+ * rev_string - print a string in reverse
+ * @s: provided variable
+ * Return: void
  */
-void reset_to_98(int *n)
+void rev_string(char *s)
 {
-	int n;
-	int n = 98;
-	int *p = &n;
-	*p = 402;
+	int n = 0, i = 0;
+	char c[2000];
+
+	while (*(s + n))
+	{
+		*(c + n) = *(s + n);
+		n++;
+	}
+	n = n - 1;
+	while (n >= 0)
+	{
+		*(s + n) = *(c + i);
+		i++;
+		n--;
+	}
 }
